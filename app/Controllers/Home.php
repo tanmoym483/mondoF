42622d0835af5c14ab5f00a926965afa6c0bd9f7
tanmoym483 <tanmoym483@gmail.com>
2025-10-20 18:59:30 +0530

    some changes on master page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,11 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        $data = [
-            'title' => 'Home Page',
-            'message' => 'Welcome to the Food E-commerce Dashboard!'
-        ];
-
-        return $this->render('home', $data);
+        return $this->render('home');
     }
 }
--- app/Views/layouts/master.php
+++ app/Views/layouts/master.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= esc($data['title'] ?? 'My Site') ?></title>
+  <title>mondoF</title>
   <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
   
 </head>
